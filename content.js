540705c82a9378bf2660499fe437924b1daf9236
Denys Lonkovski Maiolli <denysmaiolli@hotmail.com>
2020-12-02 17:47:33 -0300

    Audios are already accelerated on click in 2x button

---
+++ content.js
@@ -0,0 +1,24 @@
+const reqHeader = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const header = document.querySelector("._2O84H");
+        resolve(header)
+
+        const button = document.createElement("button");
+        button.innerHTML = "2x";
+        button.classList.add("twoTimesButton");
+
+
+        button.addEventListener('click', ()=>{
+            const audios = document.querySelectorAll("audio");
+            audios.forEach((audio) => {
+                audio.playbackRate = 2;
+            })
+        })
+
+        header.appendChild(button)
+    }, 5000)
+})
+
+reqHeader.then(value => {
+
+})
